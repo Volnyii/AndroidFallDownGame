4d66e61e68dbf7807662e8c837c04a7261cd8af2
Markov <boratboroda@yandex.ru>
2023-06-24 22:42:59 +0300

    Перелопачен гитигнор, добавлены утерянные файлы

---
+++ AndroidGame/Assets/Scripts/GameCutscene/GameCutsceneStarter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public enum GameState : byte
+{
+    None = 0,
+    CutScene1 = 1,
+    Game = 2,
+    CutScene2 = 3
+}
+public class GameCutsceneStarter : MonoBehaviour
+{
+    [SerializeField] private GameState _gameState;
+
+    
+    [SerializeField] private PlayableDirector _startCutscene;
+    [SerializeField] private PlayableDirector _endCutscene;
+    [SerializeField] private CinemachineVirtualCamera _cutscene1_VC;
+    [SerializeField] private CinemachineVirtualCamera _cutscene2_VC;
+    [SerializeField] private GameObject[] _gameActiveObjects;
+
+    private TimeLine _timeLine;
+    private MainCamera _mainCamera;
+    private PlayerVerticalSpeed _verticalSpeed;
+
+    private void Start()
+    {
+        _mainCamera = FindObjectOfType<MainCamera>();
+        _timeLine = FindObjectOfType<TimeLine>();
+        _verticalSpeed = FindObjectOfType<PlayerVerticalSpeed>();
+        _timeLine.OnGameWasEnd += StartCutscene_2;
+        _startCutscene.played += OnCutscene1_Ended;
+        _endCutscene.played += OnCutscene2_Ended;
+        
+        StartCutscene_1();
+    }
+
+    private void OnCutscene2_Ended(PlayableDirector obj)
+    {
+        // show finish UI after game end 
+    }
+
+    private void OnCutscene1_Ended(PlayableDirector obj)
+    {
+        StartGameAfter_Cutscene1();
+    }
+
+    private void StartCutscene_1()
+    {
+        _startCutscene.Play();
+        _cutscene1_VC.Priority = 1000;
+        SetGameEnvironment(false);
+        _mainCamera.enabled = false;
+        _verticalSpeed.enabled = false;
+        _gameState = GameState.CutScene1;
+    }
+
+    private void SetGameEnvironment(bool state)
+    {
+        foreach (var obj in _gameActiveObjects)
+        {
+            obj.SetActive(state);
+        }
+    }
+
+    private void StartGameAfter_Cutscene1()
+    {
+        _cutscene1_VC.Priority = 0;
+        SetGameEnvironment(true);
+        _mainCamera.enabled = true;
+        _verticalSpeed.enabled = true;
+        _gameState = GameState.Game;
+    }
+    
+    private void StartCutscene_2()
+    {
+        _endCutscene.Play();
+        _cutscene2_VC.Priority = 1000;
+        SetGameEnvironment(false);
+        _mainCamera.enabled = false;
+        _verticalSpeed.enabled = false;
+        _gameState = GameState.CutScene2;
+    }
+}
--- AndroidGame/Assets/Scripts/MainCamera.cs
+++ AndroidGame/Assets/Scripts/MainCamera.cs
@@ -1,30 +1,48 @@
 ﻿using System;
 using System.Collections;
+using Cinemachine;
 using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    [SerializeField] private float _flequencyNewValue = 5f;
     [SerializeField] private float _fadeTime = 1.5f;
     [SerializeField] private SpriteRenderer _player;
     [SerializeField] private float _minZoom;
     [SerializeField] private float _maxZoom;
     private Vector3 _offset;
     private Camera _camera;
+    private float _flequencyOldValue = 1f;
     private PlayerVerticalSpeed _playerVerticalSpeed;
 
     private Coroutine _cameraFade;
     private bool _lastZoomState;
 
+    private CinemachineBasicMultiChannelPerlin _nc;
+    private CinemachineVirtualCamera _currentVC;
+
     public event Action OnCameraWasChanged;
     
-    private void Awake()
+    private void OnEnable()
     {
         _playerVerticalSpeed = FindObjectOfType<PlayerVerticalSpeed>();
         _playerVerticalSpeed.OnGearChanged += SetCameraMinMaxValue;
         _offset = transform.position - _player.transform.position;
         _camera = GetComponent<Camera>();
     }
-    
+
+    private void OnDisable()
+    {
+        _playerVerticalSpeed.OnGearChanged -= SetCameraMinMaxValue;
+    }
+
+    private void Start()
+    {
+        var cameraBrain = FindObjectOfType<CinemachineBrain>();
+        _currentVC = cameraBrain.ActiveVirtualCamera as CinemachineVirtualCamera;
+        _nc = _currentVC.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
     private void SetCameraMinMaxValue(int gear)
     {
         bool zoomIn = gear >= 2;
@@ -33,6 +51,7 @@ public class MainCamera : MonoBehaviour
         
         _lastZoomState = zoomIn;
         var size = zoomIn ? _maxZoom : _minZoom;
+        _nc.m_FrequencyGain = zoomIn ? _flequencyNewValue : _flequencyOldValue;
         SetCameraSize(size);
     }
 
@@ -48,13 +67,13 @@ public class MainCamera : MonoBehaviour
 
     private IEnumerator SetOrthographicSizeInternal(float size, float time)
     {
-        var startValue = _camera.orthographicSize;
+        var startValue = _currentVC.m_Lens.OrthographicSize;
         var endValue = size;
         var currentTime = 0f;
         while (currentTime < time)
         {
             OnCameraWasChanged?.Invoke();
-            _camera.orthographicSize = Mathf.Lerp(startValue, endValue, currentTime / time);
+            _currentVC.m_Lens.OrthographicSize = Mathf.Lerp(startValue, endValue, currentTime / time);
             currentTime += Time.deltaTime;
             yield return null;
         }
